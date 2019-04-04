2b8d0555d2b20d8b20d6c13961af980f67961d9a
MatiasRT <ruiztorresmatias@gmail.com>
2019-04-03 21:55:58 -0300

    Commit #1
    - game created

---
+++ Fisica-TP1/Assets/Scripts/CanonMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonMovement : MonoBehaviour
+{
+    [SerializeField] GameObject canon;
+    [SerializeField] string input;
+    [SerializeField] float speed;
+
+    private void Update()
+    {
+        float inputY = Input.GetAxis(input);
+
+        canon.transform.Rotate(0, 0, -inputY * Time.deltaTime * speed);
+    }
+}
---
+++ Fisica-TP1/Assets/Scripts/TankMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    [SerializeField] float velInicial;
+    [SerializeField] float vel;
+    [SerializeField] string input;
+
+    void Update()
+    {
+        float inputX = Input.GetAxis(input);
+
+        if (inputX > 0)
+            transform.position = new Vector3(transform.position.x + (velInicial + (vel * Time.deltaTime)), transform.position.y, 0);
+
+        if (inputX < 0)
+            transform.position = new Vector3(transform.position.x - (velInicial + (vel * Time.deltaTime)),transform.position.y, 0);
+    }
+}
