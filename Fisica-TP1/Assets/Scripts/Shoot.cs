3f8bb287d0d55003eb4ddbe94bc91139aa7e267a
Matias <ruiztorresmatias@gmail.com>
2019-04-10 11:45:40 -0300

    Commit #2
    - Shoot configured

---
+++ Fisica-TP1/Assets/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform canon;
+
+    void Start()
+    {
+        canon = GetComponent<Transform>();    
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Transform tform = Instantiate(bullet, canon.transform.position, Quaternion.identity).transform;
+            tform.eulerAngles += transform.eulerAngles;
+            //Instantiate(bullet, canon.transform.position, Quaternion.identity);
+            tform.GetComponent<BulletMovement>().CalculateVelocity(transform.eulerAngles.y);
+
+            Debug.Log("crea bala");
+        }
+    }
+}
