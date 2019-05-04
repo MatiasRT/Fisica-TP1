a33f4195f55c995233b2785bfd3bfc506e24a944
Matias <ruiztorresmatias@gmail.com>
2019-05-04 19:27:26 -0300

    Commit #12
    - Clamp rotation for canon

---
+++ Fisica-TP1/Assets/Scripts/General/Clamp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clamp : MonoBehaviour
+{
+    [SerializeField] float minRot;
+    [SerializeField] float maxRot;
+
+    private float x, y, z = 0;
+
+    void Start()
+    {
+        x = transform.eulerAngles.x;
+        y = transform.eulerAngles.y;
+        z = Mathf.Clamp(transform.eulerAngles.z, minRot, maxRot);
+        transform.eulerAngles= new Vector3(x,y,z);
+    }
+
+    void Update()
+    {
+        if(transform.eulerAngles.z > maxRot || transform.eulerAngles.z < minRot)
+        {
+            x = transform.eulerAngles.x;
+            y = transform.eulerAngles.y;
+            z = Mathf.Clamp(transform.eulerAngles.z, minRot, maxRot);
+            transform.eulerAngles= new Vector3(x,y,z);
+        }
+    }
+}
